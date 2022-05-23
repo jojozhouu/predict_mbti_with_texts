582cb08bf4450c256aa0460ec1a99773df150080
Jojo Zhou <jojozhouu@outlook.com>
2022-05-23 17:51:43 -0500

    updated log messages

--- src/load_save_data_s3.py
+++ src/load_save_data_s3.py
@@ -1,11 +1,15 @@
 # Data preprocessing processes, including getting raw data from data/raw/, uploading to S3 bucket (optional, included for reference),
 # and preprocessing XXX # TODO
 
+import logging
 import os
 import os.path
+import sys
 from typing import Tuple
 import boto3
 import re
+
+logger = logging.getLogger(__name__)
 
 
 def parse_s3(s3path: str) -> Tuple[str, str]:  # TODO: NEED TO TEST THIS
@@ -44,10 +48,11 @@ def upload_file_to_s3(local_path: str, s3path: str) -> None:
 
     try:
         bucket.upload_file(local_path, s3_just_path)
-        # TODO, logger.info('Data uploaded from %s to %s', local_path, s3path)
+        logger.info('Data uploaded from %s to %s', local_path, s3path)
     except boto3.exceptions.S3UploadFailedError as se:
-        # TODO, add logging for reporting errors
-        raise se
+        logger.error(
+            """Upload failed. Please check if the file exists and if the S3 path is correct. %s""", se)
+        sys.exit(1)
 
 
 def download_file_from_s3(local_path: str, s3path: str) -> None:
@@ -62,25 +67,23 @@ def download_file_from_s3(local_path: str, s3path: str) -> None:
                             aws_access_key_id=os.environ.get(
                                 "AWS_ACCESS_KEY_ID"),
                             aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"))
-        # TODO logger.info("AWS S3 bucket connected.")
+        logger.info("S3 bucket %s connected.", s3bucket)
     except botocore.exceptions.PartialCredentialsError as pe:  # QUESTION except here or after upload
-        # TODO logger.error("Invalid AWS credentials")
+        logger.error("Invalid AWS credentials")
         raise pe
     except botocore.exceptions.NoCredentialsError as ce:  # check if it is this error
-        # TODO logger.error("""No AWS credentials detected in env. Please provide credentials via AWS_ACCESS_KEY_ID and
+        logger.error("""No AWS credentials detected in env. Please provide credentials via AWS_ACCESS_KEY_ID and
         # AWS_SECRET_ACCESS_KEY env variables""")
         raise ce
-
-    bucket = s3.Bucket(s3bucket)
-
-    # TODO logging: successfully connected
+    else:
+        bucket = s3.Bucket(s3bucket)
 
     try:
         bucket.download_file(s3_just_path, local_path)
     except botocore.exceptions.NoCredentialsError as ne:
-        # logger.error(
-        #    'Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
+        logger.error(
+            'Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
         raise ne
     else:
-        # logger.info('Data downloaded from %s to %s', s3path, local_path)
-        pass
+        logger.info('Data successfully downloaded from %s to %s',
+                    s3path, local_path)
