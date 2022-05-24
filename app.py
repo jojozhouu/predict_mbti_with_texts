856ff87648fe8caa626cdc6206a76cc301bdd8cf
Jojo Zhou <jojozhouu@outlook.com>
2022-05-24 01:14:27 -0500

    modified app.py to to project-specific draft version; added corresponding html templates

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import sqlalchemy.exc
 from flask import Flask, render_template, request, redirect, url_for
 
 # For setting up the Flask-SQLAlchemy database session
-from src.add_songs import Tracks, TrackManager
+from src.manage_rds_db import PostManager
 
 # Initialize the Flask application
 app = Flask(__name__, template_folder="app/templates",
@@ -25,75 +25,103 @@ logger.debug(
     'in config/flaskconfig.py (e.g. `-p 5000:5000`). Otherwise, go to the '
     'port defined on the left side of the port mapping '
     '(`i.e. -p THISPORT:5000`). If you are running from a Windows machine, '
-    'go to 127.0.0.1 instead of 0.0.0.0.', app.config["HOST"]
-    , app.config["PORT"])
+    'go to 127.0.0.1 instead of 0.0.0.0.', app.config["HOST"], app.config["PORT"])
 
 # Initialize the database session
-track_manager = TrackManager(app)
+track_manager = PostManager(app)
 
 
 @app.route('/')
-def index():
-    """Main view that lists songs in the database.
-
-    Create view into index page that uses data queried from Track database and
-    inserts it into the app/templates/index.html template.
+def home():
+    """Main view that displays all 16 MBTI types and a button to input_text page
 
     Returns:
         Rendered html template
-
     """
+    return render_template('index.html')
 
-    try:
-        tracks = track_manager.session.query(Tracks).limit(
-            app.config["MAX_ROWS_SHOW"]).all()
-        logger.debug("Index page accessed")
-        return render_template('index.html', tracks=tracks)
-    except sqlite3.OperationalError as e:
-        logger.error(
-            "Error page returned. Not able to query local sqlite database: %s."
-            " Error: %s ",
-            app.config['SQLALCHEMY_DATABASE_URI'], e)
-        return render_template('error.html')
-    except sqlalchemy.exc.OperationalError as e:
-        logger.error(
-            "Error page returned. Not able to query MySQL database: %s. "
-            "Error: %s ",
-            app.config['SQLALCHEMY_DATABASE_URI'], e)
-        return render_template('error.html')
-    except:
-        traceback.print_exc()
-        logger.error("Not able to display tracks, error page returned")
-        return render_template('error.html')
+# @app.route('/')
+# def index():
+#     """Main view that displays all 16 MBTI types and a text box for entering texts.
+
+#     Returns:
+#         Rendered html template
+#     """
+
+#     try:
+#         tracks = track_manager.session.query(Tracks).limit(
+#             app.config["MAX_ROWS_SHOW"]).all()
+#         logger.debug("Index page accessed")
+#         return render_template('index.html', tracks=tracks)
+#     except sqlite3.OperationalError as e:
+#         logger.error(
+#             "Error page returned. Not able to query local sqlite database: %s."
+#             " Error: %s ",
+#             app.config['SQLALCHEMY_DATABASE_URI'], e)
+#         return render_template('error.html')
+#     except sqlalchemy.exc.OperationalError as e:
+#         logger.error(
+#             "Error page returned. Not able to query MySQL database: %s. "
+#             "Error: %s ",
+#             app.config['SQLALCHEMY_DATABASE_URI'], e)
+#         return render_template('error.html')
+#     except:
+#         traceback.print_exc()
+#         logger.error("Not able to display tracks, error page returned")
+#         return render_template('error.html')
 
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    """View that process a POST with new song input
+@app.route('/input_text', methods=['Get', 'POST'])
+def input_text():
+    """View that process a text box for entering texts.
 
     Returns:
-        redirect to index page
+        redirect to result page
     """
+    # Get the text from the text box
+    text = request.form.get('text')
+    # TODO call preprocess and predict functions
+    return render_template('search.html')
+    # try:
+    #     track_manager.add_track(artist=request.form['artist'],
+    #                             album=request.form['album'],
+    #                             title=request.form['title'])
+    #     logger.info("New song added: %s by %s", request.form['title'],
+    #                 request.form['artist'])
+    #     return redirect(url_for('index'))
+    # except sqlite3.OperationalError as e:
+    #     logger.error(
+    #         "Error page returned. Not able to add song to local sqlite "
+    #         "database: %s. Error: %s ",
+    #         app.config['SQLALCHEMY_DATABASE_URI'], e)
+    #     return render_template('error.html')
+    # except sqlalchemy.exc.OperationalError as e:
+    #     logger.error(
+    #         "Error page returned. Not able to add song to MySQL database: %s. "
+    #         "Error: %s ",
+    #         app.config['SQLALCHEMY_DATABASE_URI'], e)
+    #     return render_template('error.html')
 
-    try:
-        track_manager.add_track(artist=request.form['artist'],
-                                album=request.form['album'],
-                                title=request.form['title'])
-        logger.info("New song added: %s by %s", request.form['title'],
-                    request.form['artist'])
-        return redirect(url_for('index'))
-    except sqlite3.OperationalError as e:
-        logger.error(
-            "Error page returned. Not able to add song to local sqlite "
-            "database: %s. Error: %s ",
-            app.config['SQLALCHEMY_DATABASE_URI'], e)
-        return render_template('error.html')
-    except sqlalchemy.exc.OperationalError as e:
-        logger.error(
-            "Error page returned. Not able to add song to MySQL database: %s. "
-            "Error: %s ",
-            app.config['SQLALCHEMY_DATABASE_URI'], e)
-        return render_template('error.html')
+
+@app.route("/result", methods=['GET', 'POST'])
+def show_result():
+    """View that displays the result of the text prediction.
+
+    Returns:
+        Rendered html template
+    """
+    return render_template('result.html')
+
+
+@app.route('/learn_more')
+def learn_more():
+    """View that process a text box for entering texts.
+
+    Returns:
+        redirect to result page
+    """
+    # TODO call preprocess and predict functions
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
